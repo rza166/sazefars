587256c62a7d48e808f8f1161bf7710938ebc95f
zabiRahimi <rahimi.z1360@gmail.com>
2020-02-13 10:55:37 +0330

    edit html form login.php

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -2,66 +2,74 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>صفحه ورود</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="/css/admin/login.css">
   </head>
   <body>
-    {{-- <form method="POST" action="{{ route('login') }}">
-        @csrf
+    <div class="content_loginAdmin">
+      <div class="content_loginAdmin2">
 
-        <div class="form-group row">
-            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-            <div class="col-md-6">
-                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                @error('email')
-                    <span class="invalid-feedback" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                @enderror
-            </div>
+        <div class="headerLogin">
+          خوش آمدید
         </div>
+        <form method="POST" action="{{ route('admin.login.submit') }}">
+            @csrf
+            <div class="form-group ">
+                <label for="username" class=" "> نام کاربری یا موبایل</label>
 
-        <div class="form-group row">
-            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                <div class="">
+                    <input id="username" type="text" class="form-control @error('username') is-invalid @enderror" name="username" value="{{ old('username') }}" required autocomplete="username"oninvalid="this.setCustomValidity('لطفا نام کاربری یا موبایل خود را وارد کنید ')" oninput="setCustomValidity('')" autofocus>
 
-            <div class="col-md-6">
-                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                    @error('username')
+                      <span class="errorM" role="alert">
+                          {{ $message }}
+                      </span>
+                    @enderror
 
-                @error('password')
-                    <span class="invalid-feedback" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                @enderror
-            </div>
-        </div>
-
-        <div class="form-group row">
-            <div class="col-md-6 offset-md-4">
-                <div class="form-check">
-                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                    <label class="form-check-label" for="remember">
-                        {{ __('Remember Me') }}
-                    </label>
                 </div>
             </div>
-        </div>
 
-        <div class="form-group row mb-0">
-            <div class="col-md-8 offset-md-4">
-                <button type="submit" class="btn btn-primary">
-                    {{ __('Login') }}
-                </button>
+            <div class="form-group ">
+                <label for="password" class="">رمز عبور</label>
 
-                @if (Route::has('password.request'))
-                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                        {{ __('Forgot Your Password?') }}
-                    </a>
-                @endif
+                <div class="">
+                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required oninvalid="this.setCustomValidity('لطفا رمز عبور خود را وارد کنید')" oninput="setCustomValidity('')" autocomplete="current-password">
+
+                    @error('password')
+                        <span class="errorM" role="alert">
+                            {{ $message }}
+                        </span>
+                    @enderror
+                </div>
             </div>
-        </div>
-    </form> --}}
+
+            <div class="form-group ">
+                    <div class="form-check">
+                      <label class="form-check-label" for="remember">
+                          {{ __('مرا به یاد داشته باش ') }}
+                      </label>
+                        <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                    </div>
+            </div>
+
+            {{-- <div class="form-group "> --}}
+                {{-- <div class=""> --}}
+                    <button type="submit" class="btn btn-primary">
+                        {{ __('ورود') }}
+                    </button>
+
+                    @if (Route::has('password.request'))
+                        <a class="btn btn-link" href="{{ route('password.request') }}">
+                            {{ __('رمز عبور خود را فراموش کرده ام ؟') }}
+                        </a>
+                    @endif
+                {{-- </div> --}}
+            {{-- </div> --}}
+        </form>
+      </div>
+    </div>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/js/bootstrap.min.js"></script>
   </body>
 </html>
